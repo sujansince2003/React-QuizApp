c28262d105f14653b9ecc66dcb887f2788065cf7
sujan khatri <sujansince2059@gmail.com>
2023-08-29 21:45:49 +0545

    selecting answer

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -1,17 +1,14 @@
 const Options = ({question,dispatch,answer}) => {
+   const hasAnswered=answer !==null;
     return (  
         <div className="options">
         {question.options.map((option,index)=>
-        (<button 
-            className={`btn btn-option  ${index===answer ? "answer": "" }        `}
+          (<button 
+            className={`btn btn-option    ${index===answer ? "answer": "" } ${hasAnswered? index===question.correctOption? "correct":"wrong":""} `}
+            disabled={hasAnswered}
             key={index}
-            onClick={()=>dispatch({type:"newAnswer",payload:index})
-        
-        }
-        
-        
-        
-        >{option}</button>))}
+            onClick={()=>dispatch({type:"newAnswer",payload:index})}
+           >{option}</button>))}
     </div>
     );
 }
