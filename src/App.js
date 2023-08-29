3807f0cfb327cccb059d989be98bef148101a8e5
sujan khatri <sujansince2059@gmail.com>
2023-08-29 14:36:56 +0545

    using json server script

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import Main from "./Main";
 function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
